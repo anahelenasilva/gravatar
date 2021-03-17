42b92fe2c4de73cef0fcb4fe704d186b6ab46d83
Ana Helena <anahelenarp@hotmail.com>
2021-03-17 15:32:59 -0300

    Adicionar tamanho da imagem na extensão #1

--- Gravatar/GravatarExtension.cs
+++ Gravatar/GravatarExtension.cs
@@ -5,7 +5,7 @@ namespace Gravatar
 {
     public static class GravatarExtension
     {
-        public static string ToGravatar(this string email)
+        public static string ToGravatar(this string email, int size = 80)
         {
             if (string.IsNullOrEmpty(email))
             {
@@ -22,7 +22,7 @@ namespace Gravatar
                 sb.Append(t.ToString("X2"));
             }
 
-            return $"https://www.gravatar.com/avatar/{sb.ToString().ToLower()}";
+            return $"https://www.gravatar.com/avatar/{sb.ToString().ToLower()}?s={size}";
         }
     }
 }
