abcd9747dcb549d01f6076280f4eee76c4ddbfff
Ana Helena <anahelenarp@hotmail.com>
2021-03-17 15:46:44 -0300

    add CI/CD

--- Gravatar.Tests/GravatarExtensionTests.cs
+++ Gravatar.Tests/GravatarExtensionTests.cs
@@ -7,16 +7,18 @@ namespace Gravatar.Tests
     {
         [TestMethod("Should validate gravatar extension")]
         [TestCategory("Gravatar Tests")]
-        [DataRow(null, false)]
-        [DataRow("", false)]
-        [DataRow(" ", false)]
-        [DataRow("a@a", false)]
-        [DataRow("a@ana.com", false)]
-        [DataRow("anahelenarp@hotmail.com", true)]
-        public void ShouldValidateGravatar(string email, bool status)
+        [DataRow(null, 200, false)]
+        [DataRow("", 200, false)]
+        [DataRow(" ", 200, false)]
+        [DataRow("a@a", 200, false)]
+        [DataRow("a@ana.com", 200, false)]
+        [DataRow("anahelenarp@hotmail.com", null, true)]
+        [DataRow("anahelenarp@hotmail.com", 200, true)]
+        public void ShouldValidateGravatar(string email, int? size, bool status)
         {
-            var result = "https://www.gravatar.com/avatar/0b9f9e883ee0a57d9130fae9650a9e18";
-            Assert.AreEqual(email.ToGravatar() == result, status);
+            var imageSize = size != null ? size.Value.ToString() : "80";
+            var result = $"https://www.gravatar.com/avatar/0b9f9e883ee0a57d9130fae9650a9e18?s={imageSize}";
+            Assert.AreEqual(email.ToGravatar(size ?? 80) == result, status);
         }
     }
 }
